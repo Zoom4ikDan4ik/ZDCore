3ce386ad0ddb579eb2d28a66611e7468a2e73cda
Zoom4ikDan4ik <zoom4ikdan4ik@gmail.com>
2020-05-31 20:22:47 +0300

    ZDCore V6.0

--- src/ru/zoom4ikdan4ik/core/api/RegistrationAPI.java
+++ src/ru/zoom4ikdan4ik/core/api/RegistrationAPI.java
@@ -18,7 +18,7 @@ public class RegistrationAPI implements IBase {
     /**
      * Record all of the plugins that work from ZDCore
      */
-    public static final Map<Plugin, ICore> plugins = new HashMap<>();
+    private static final Map<Plugin, ICore> plugins = new HashMap<>();
 
     public static final Map<Plugin, ICore> getPlugins() {
         return plugins;
@@ -31,43 +31,52 @@ public class RegistrationAPI implements IBase {
         if (!corePlugin.getName().equalsIgnoreCase(plugin.getName()))
             plugins.put(plugin, manager);
 
-        IConfigManager config = manager.getConfigManager();
-        if (config != null) {
+        registerConfigManager(plugin, manager.getConfigManager());
+        registerSQLManager(plugin, manager.getSQLManager());
+        registerSchedulerManager(plugin, manager.getSchedulerManager());
+        registerCommandManager(plugin, manager.getCommandManager());
+    }
+
+    public final static void registerConfigManager(Plugin plugin, IConfigManager configManager) {
+        if (configManager != null) {
             loggerUtils.info(plugin, "Loading configs...");
 
-            config.setFileConfig(coreMethods.createConfigYML(plugin.getName(), plugin));
-            config.setConfig(YamlConfiguration.loadConfiguration(config.getFileConfig()));
-            config.loadConfig();
-            config.save();
+            configManager.setFileConfig(coreMethods.createConfigYML(plugin.getName(), plugin));
+            configManager.setConfig(YamlConfiguration.loadConfiguration(configManager.getFileConfig()));
+            configManager.loadConfig();
+            configManager.save();
         }
+    }
 
-        ISQLManager sql = manager.getSQLManager();
-        if (sql != null) {
+    public final static void registerSQLManager(Plugin plugin, ISQLManager sqlManager) {
+        if (sqlManager != null) {
             loggerUtils.info(plugin, "Create tables...");
 
-            sql.createTables();
+            sqlManager.createTables();
         }
+    }
 
-        ISchedulerManager scheduler = manager.getSchedulerManager();
-        if (scheduler != null) {
+    public final static void registerSchedulerManager(Plugin plugin, ISchedulerManager schedulerManager) {
+        if (schedulerManager != null) {
             loggerUtils.info(plugin, "Add schedulers...");
 
-            scheduler.addScheduler();
+            schedulerManager.addScheduler();
         }
+    }
 
-        ICommandManager command = manager.getCommandManager();
-        if (command != null) {
+    public final static void registerCommandManager(Plugin plugin, ICommandManager commandManager) {
+        if (commandManager != null) {
             loggerUtils.info(plugin, "Register commands...");
 
-            for (String com : command.getCommands()) {
-                PluginCommand pluginCommand = Bukkit.getPluginCommand(com);
+            for (String command : commandManager.getCommands()) {
+                PluginCommand pluginCommand = Bukkit.getPluginCommand(command);
 
                 if (pluginCommand.isRegistered()) {
-                    loggerUtils.info(plugin, "Command %% was registered by %% plugin", com, pluginCommand.getPlugin().getName());
+                    loggerUtils.info(plugin, "Command %% was registered by %% plugin", command, pluginCommand.getPlugin().getName());
 
-                    Bukkit.getPluginCommand(com).setExecutor(command);
+                    Bukkit.getPluginCommand(command).setExecutor(commandManager);
                 } else
-                    loggerUtils.info(plugin, "Command %% can't be register by %% plugin", com, pluginCommand.getPlugin().getName());
+                    loggerUtils.info(plugin, "Command %% can't be register by %% plugin", command, pluginCommand.getPlugin().getName());
             }
         }
     }
